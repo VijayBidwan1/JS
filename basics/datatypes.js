b1982b16be059a61facd05a29480f0b4526cd876
vijay bidwan <111508714+VijayBidwan1@users.noreply.github.com>
2024-07-11 20:16:37 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+// "use strict" : treat all JS code as newer version
+
+// alert(3+3)
+
+// when to use " ; " just for code readability this will not show error 
+
+// you will get documentation of js on " MDN and ecma " website
+
+
+
+/* 
+Datatypes 
+int 
+bigint
+string
+boolean
+null : value is 0    "null is a object " when you use typeof
+undefined: value not given
+symbol => unique
+*/
+
+let name="vijay"  // string
+let age=19        // int
+let isLoggedIn=false   // boolean
+
+console.log(typeof age)
---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId=1444
+let accountEmail="vijaybidwan9@gmail.com"
+var accountPassword="12345"
+accountCity="indore"
+
+// accountId=345   This is constant so we can't change
+
+/* we can declare variable using var and let keywords  var have scoping problem therefore we use let
+ keyword therefore don;t use var */
+
+accountEmail="vijay9@gmail.com"
+accountPassword="1333"
+
+let accountState;
+
+// accountState is undefine because there is no value assigned to it
+
+console.log(accountState)
+console.table([accountId,accountEmail,accountPassword,accountCity])
